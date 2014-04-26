80d889ac641f0c0793c8b8f3335ab68cb6e85c94
Ali Gholami Rudi <ali@rudi.ir>
2014-04-26 19:41:19 +0430

    gen.sh: make afmconv() generic

--- gen.sh
+++ gen.sh
@@ -24,7 +24,7 @@ echo "unitwidth 10" >>$TP/DESC
 function afmconv
 {
 	echo $1
-	cat $FP/$3 | ./mktrfn $4 -a -b -r$RES -t $1 -p $2 >$TP/$1
+	cat $3 | ./mktrfn $4 -a -b -r$RES -t $1 -p $2 >$TP/$1
 }
 
 # converting otf and ttf fonts; needs fontforge or lcdf-typetools
@@ -35,7 +35,7 @@ function otfconv
 	if [ "$USELCDF" == "1" ]; then
 		cat $2 | cfftot1 2>/dev/null | t1rawafm >$AFM 2>/dev/null
 	else
-		echo -e "Open (\"$2\")\nGenerate(\"$AFM\")" | fontforge >/dev/null 2>&1
+		echo -e "Open(\"$2\")\nGenerate(\"$AFM\")" | fontforge >/dev/null 2>&1
 	fi
 	cat $AFM | ./mktrfn -b -a -r$RES -t $FN -p $FN -k50 | \
 		sed "/^ligatures /s/ $LIGIGN//g" >$TP/$FN
@@ -43,45 +43,45 @@ function otfconv
 }
 
 # The standard fonts
-afmconv R	Times-Roman		n021003l.afm
-afmconv I	Times-Italic		n021023l.afm
-afmconv B	Times-Bold		n021004l.afm
-afmconv BI	Times-BoldItalic	n021024l.afm
-afmconv S	Symbol			s050000l.afm -s
-afmconv S1	Times-Roman		n021003l.afm -s
-afmconv AR	AvantGarde-Book		a010013l.afm
-afmconv AI	AvantGarde-BookOblique	a010033l.afm
-afmconv AB	AvantGarde-Demi		a010015l.afm
-afmconv AX	AvantGarde-DemiOblique	a010035l.afm
-afmconv H	Helvetica		n019043l.afm
-afmconv HI	Helvetica-Oblique	n019063l.afm
-afmconv HB	Helvetica-Bold		n019044l.afm
-afmconv HX	Helvetica-BoldOblique	n019064l.afm
-afmconv Hr	Helvetica-Narrow	n019043l.afm
-afmconv Hi	Helvetica-Narrow-Oblique	n019063l.afm
-afmconv Hb	Helvetica-Narrow-Bold	n019044l.afm
-afmconv Hx	Helvetica-Narrow-BoldOblique	n019064l.afm
-afmconv KR	Bookman-Light		b018012l.afm
-afmconv KI	Bookman-LightItalic	b018032l.afm
-afmconv KB	Bookman-Demi		b018015l.afm
-afmconv KX	Bookman-DemiItalic	b018035l.afm
-afmconv NR	NewCenturySchlbk-Roman	c059013l.afm
-afmconv NI	NewCenturySchlbk-Italic	c059033l.afm
-afmconv NB	NewCenturySchlbk-Bold	c059016l.afm
-afmconv NX	NewCenturySchlbk-BoldItalic	c059036l.afm
-afmconv PA	Palatino-Roman		p052003l.afm
-afmconv PR	Palatino-Roman		p052003l.afm
-afmconv PI	Palatino-Italic		p052023l.afm
-afmconv PB	Palatino-Bold		p052004l.afm
-afmconv PX	Palatino-BoldItalic	p052024l.afm
-afmconv C	Courier			n022003l.afm
-afmconv CO	Courier			n022003l.afm
-afmconv CW	Courier			n022003l.afm
-afmconv CI	Courier-Oblique		n022023l.afm
-afmconv CB	Courier-Bold		n022004l.afm
-afmconv CX	Courier-BoldOblique	n022024l.afm
-afmconv ZI	ZapfChancery-MediumItalic	z003034l.afm
-afmconv ZD	ZapfDingbats		d050000l.afm
+afmconv R	Times-Roman		$FP/n021003l.afm
+afmconv I	Times-Italic		$FP/n021023l.afm
+afmconv B	Times-Bold		$FP/n021004l.afm
+afmconv BI	Times-BoldItalic	$FP/n021024l.afm
+afmconv S	Symbol			$FP/s050000l.afm -s
+afmconv S1	Times-Roman		$FP/n021003l.afm -s
+afmconv AR	AvantGarde-Book		$FP/a010013l.afm
+afmconv AI	AvantGarde-BookOblique	$FP/a010033l.afm
+afmconv AB	AvantGarde-Demi		$FP/a010015l.afm
+afmconv AX	AvantGarde-DemiOblique	$FP/a010035l.afm
+afmconv H	Helvetica		$FP/n019043l.afm
+afmconv HI	Helvetica-Oblique	$FP/n019063l.afm
+afmconv HB	Helvetica-Bold		$FP/n019044l.afm
+afmconv HX	Helvetica-BoldOblique	$FP/n019064l.afm
+afmconv Hr	Helvetica-Narrow	$FP/n019043l.afm
+afmconv Hi	Helvetica-Narrow-Oblique	$FP/n019063l.afm
+afmconv Hb	Helvetica-Narrow-Bold	$FP/n019044l.afm
+afmconv Hx	Helvetica-Narrow-BoldOblique	$FP/n019064l.afm
+afmconv KR	Bookman-Light		$FP/b018012l.afm
+afmconv KI	Bookman-LightItalic	$FP/b018032l.afm
+afmconv KB	Bookman-Demi		$FP/b018015l.afm
+afmconv KX	Bookman-DemiItalic	$FP/b018035l.afm
+afmconv NR	NewCenturySchlbk-Roman	$FP/c059013l.afm
+afmconv NI	NewCenturySchlbk-Italic	$FP/c059033l.afm
+afmconv NB	NewCenturySchlbk-Bold	$FP/c059016l.afm
+afmconv NX	NewCenturySchlbk-BoldItalic	$FP/c059036l.afm
+afmconv PA	Palatino-Roman		$FP/p052003l.afm
+afmconv PR	Palatino-Roman		$FP/p052003l.afm
+afmconv PI	Palatino-Italic		$FP/p052023l.afm
+afmconv PB	Palatino-Bold		$FP/p052004l.afm
+afmconv PX	Palatino-BoldItalic	$FP/p052024l.afm
+afmconv C	Courier			$FP/n022003l.afm
+afmconv CO	Courier			$FP/n022003l.afm
+afmconv CW	Courier			$FP/n022003l.afm
+afmconv CI	Courier-Oblique		$FP/n022023l.afm
+afmconv CB	Courier-Bold		$FP/n022004l.afm
+afmconv CX	Courier-BoldOblique	$FP/n022024l.afm
+afmconv ZI	ZapfChancery-MediumItalic	$FP/z003034l.afm
+afmconv ZD	ZapfDingbats		$FP/d050000l.afm
 
 # For otf and ttf files, we assume the postscript name of the font
 # can be obtained by dropping its extension.  Otherwise, remove the
