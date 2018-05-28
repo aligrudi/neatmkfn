1c5e9a65763249d2cd2370d288456de8f66efb16
Ali Gholami Rudi <ali@rudi.ir>
2018-05-28 17:19:38 +0430

    gen.sh: generate font descriptions from OTF files directly

--- gen.sh
+++ gen.sh
@@ -70,15 +70,15 @@ stdfont AB	$FP/a010015l.afm $FP/URWGothic-Demi.afm		-pAvantGarde-Demi
 stdfont AX	$FP/a010035l.afm $FP/URWGothic-DemiOblique.afm	-pAvantGarde-DemiOblique
 stdfont HR	$FP/n019003l.afm $FP/NimbusSans-Regular.afm	-pHelvetica
 stdfont HI	$FP/n019023l.afm $FP/NimbusSans-Oblique.afm	-pHelvetica-Oblique
-stdfont HB	$FP/n019004l.afm $FP/NimbusSans-Bold.afm		-pHelvetica-Bold
+stdfont HB	$FP/n019004l.afm $FP/NimbusSans-Bold.afm	-pHelvetica-Bold
 stdfont HX	$FP/n019024l.afm $FP/NimbusSans-BoldOblique.afm	-pHelvetica-BoldOblique
 stdfont Hr	$FP/n019043l.afm $FP/NimbusSansNarrow-Regular.afm	-pHelvetica-Narrow
 stdfont Hi	$FP/n019063l.afm $FP/NimbusSansNarrow-Oblique.afm	-pHelvetica-Narrow-Oblique
 stdfont Hb	$FP/n019044l.afm $FP/NimbusSansNarrow-Bold.afm	-pHelvetica-Narrow-Bold
 stdfont Hx	$FP/n019024l.afm $FP/NimbusSansNarrow-BdOblique.afm	-pHelvetica-Narrow-BoldOblique
-stdfont KR	$FP/b018012l.afm $FP/URWBookman-Light.afm		-pBookman-Light
+stdfont KR	$FP/b018012l.afm $FP/URWBookman-Light.afm	-pBookman-Light
 stdfont KI	$FP/b018032l.afm $FP/URWBookman-LightItalic.afm	-pBookman-LightItalic
-stdfont KB	$FP/b018015l.afm $FP/URWBookman-Demi.afm		-pBookman-Demi
+stdfont KB	$FP/b018015l.afm $FP/URWBookman-Demi.afm	-pBookman-Demi
 stdfont KX	$FP/b018035l.afm $FP/URWBookman-DemiItalic.afm	-pBookman-DemiItalic
 stdfont NR	$FP/c059013l.afm $FP/C059-Roman.afm		-pNewCenturySchlbk-Roman
 stdfont NI	$FP/c059033l.afm $FP/C059-Italic.afm		-pNewCenturySchlbk-Italic
@@ -88,7 +88,7 @@ stdfont PA	$FP/p052003l.afm $FP/P052-Roman.afm		-pPalatino-Roman
 stdfont PR	$FP/p052003l.afm $FP/P052-Roman.afm		-pPalatino-Roman
 stdfont PI	$FP/p052023l.afm $FP/P052-Italic.afm		-pPalatino-Italic
 stdfont PB	$FP/p052004l.afm $FP/P052-Bold.afm		-pPalatino-Bold
-stdfont PX	$FP/p052024l.afm $FP/P052-BoldItalic.afm		-pPalatino-BoldItalic
+stdfont PX	$FP/p052024l.afm $FP/P052-BoldItalic.afm	-pPalatino-BoldItalic
 stdfont CR	$FP/n022003l.afm $FP/NimbusMonoPS-Regular.afm	-pCourier
 stdfont CI	$FP/n022023l.afm $FP/NimbusMonoPS-Italic.afm	-pCourier-Oblique
 stdfont CB	$FP/n022004l.afm $FP/NimbusMonoPS-Bold.afm	-pCourier-Bold
@@ -113,5 +113,5 @@ done
 
 find $FP/ -name '*.otf' | while read FN
 do
-	otfconv `basename $FN .otf` $FN
+	ttfconv `basename $FN .otf` $FN
 done
