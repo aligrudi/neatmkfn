580cb44518555a410f5e4d90ca1608cf68f0fcf5
Ali Gholami Rudi <ali@rudi.ir>
2026-01-01 17:14:11 +0330

    mkfn: update copyright year

--- mkfn.c
+++ mkfn.c
@@ -1,7 +1,7 @@
 /*
  * NEATMKFN - GENERATE NEATROFF FONT DESCRIPTIONS
  *
- * Copyright (C) 2012-2018 Ali Gholami Rudi <ali at rudi dot ir>
+ * Copyright (C) 2012-2026 Ali Gholami Rudi <ali at rudi dot ir>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
